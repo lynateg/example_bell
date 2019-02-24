1c7732f3526cdfd4807cb904f4cd069a89b63ece
AIChernyshev <sanyachernishev@mail.ru>
2019-02-24 22:34:32 +0300

    init commit

---
+++ src/ru/bell/lessongit/Main.java
@@ -0,0 +1,11 @@
+package ru.bell.lessongit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+	// write your code here
+        //new comment1
+
+}

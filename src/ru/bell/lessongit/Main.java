41488b9b776e28fc3c52ecac88e3dc46506ca0d4
AIChernyshev <sanyachernishev@mail.ru>
2019-02-24 22:42:04 +0300

    replace comment to write

--- src/ru/bell/lessongit/Main.java
+++ src/ru/bell/lessongit/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
-	// write your code here
-        //new comment1
+
+        //write
 
 }
